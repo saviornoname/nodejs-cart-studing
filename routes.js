3d35fc6f3c97d7c0e4dfbaac8effcd19499f2610
wasylmatwiyishyn <wasylmatwiyishyn@gmail.com>
2022-09-12 22:56:21 +0300

    Addind shop structure

--- routes.js
+++ routes.js
@@ -18,7 +18,6 @@ const requestHandler = (req, res) => {
     if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
-            console.log(chunk);
             body.push(chunk);
         });
         
